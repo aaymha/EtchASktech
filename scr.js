b99fed6fd1d3788cd9d725d3a8e9a56eb37cc98f
aaymha <szymon7710@icloud.com>
2025-01-29 00:49:19 +0100

    final touch, mods soon

--- scr.js
+++ scr.js
@@ -1,4 +1,5 @@
 const divsContainer = document.querySelector("#container");
+const gener = document.querySelector("#but");
 
 for(let i = 0; i < 16 * 16; i++) {
     const createDiv = document.createElement("div");
@@ -9,16 +10,35 @@ for(let i = 0; i < 16 * 16; i++) {
     divsContainer.appendChild(createDiv);
 }
 
-function newGrid() {
-    let userGrid = parseInt(prompt("Enter a number of squares per side for the new grid:  (limit: 100) "))
-    if(userGrid < 100) {
-        let removingChilder = document.querySelector("#container");
-        let child = removingChilder.lastElementChild;
-        while(child) {
-            removingChilder.removeChild(child);
-            child = removingChilder.lastElementChild;
-        }
+function newGrid(userGrid) {
+    let removingChilder = document.querySelector("#container");
+    let child = removingChilder.lastElementChild;
 
-        
-    } else {alert("Limit is 100!")}
+    while(child) {
+        removingChilder.removeChild(child);
+        child = removingChilder.lastElementChild;
+    }
+
+    const divSize = 960 / userGrid;
+    const gridSize = userGrid;
+
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        const userCreatedGrid = document.createElement("div");
+        userCreatedGrid.style.width = `${divSize}px`;
+        userCreatedGrid.style.height = `${divSize}px`;
+
+        userCreatedGrid.addEventListener("mouseover", () => {
+            userCreatedGrid.style.backgroundColor = "red";
+        })
+        divsContainer.appendChild(userCreatedGrid);
+    }
 }
+
+gener.addEventListener("click", () => {
+    let userGrid = prompt("Enter number for a new grid:  (limit: 100");
+    if(userGrid < 100 && userGrid > 1) {
+        newGrid(userGrid);
+    } else (alert("Wrong number!"));
+})
+
+newGrid(16);
